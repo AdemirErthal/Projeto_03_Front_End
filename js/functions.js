1d1e29aaf5b5d0c99acaedf5cc4f940d5ade3071
Ademir Erthal <ade.erthal@hotmail.com>
2022-02-01 22:32:06 -0300

    Adicionado API do google maps

---
+++ js/functions.js
@@ -0,0 +1,49 @@
+window.onload = function(){
+
+	let map;
+
+	function initialize(){
+		let mapProp = {
+			center: new google.maps.LatLng(-27.648598,-48.577423),
+			scrollwheel:false,
+			zoom:12,
+			mapTypeId:google.maps.MapTypeId.ROADMAP
+		}
+
+		map = new google.maps.Map(document.getElementById("mapa"),mapProp);
+	}
+
+	function addMarker(lat,long,icon,content,click){
+		let latLng = {'lat':lat,'lng':long};
+
+		let marker = new google.maps.Marker({
+			position:latLng,
+			map:map,
+			icon:icon
+		});
+
+		let infoWindow = new google.maps.InfoWindow({
+			content:content,
+			maxWidth:200,
+			pixelOffset: new google.maps.Size(0,20)
+		});
+
+		if(click == true){
+			google.maps.event.addListener(marker,'click',function(){
+				infoWindow.open(map,marker);	
+		});
+		}else{
+			infoWindow.open(map,marker);
+		}
+	}
+
+	initialize();
+	let conteudo = '<p style="color:black;font-size:13px;padding:10px 0;">Meu Endereço</p>';
+	addMarker(-27.616637,-48.5923228,'',conteudo,true);
+
+	setTimeout(function(){
+		map.panTo({'lat':-23.550250,'lng':-46.633309});
+		map.setZoom(8);
+	},4000);
+
+}
